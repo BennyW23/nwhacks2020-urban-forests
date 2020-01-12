678e8b3d8dfcaec266479f7b506a8cf3824fe15b
Benny Wong <12177862+BennyW23@users.noreply.github.com>
2020-01-12 09:00:43 -0800

    working locally but not on remote webserver

---
+++ routes/api.js
@@ -0,0 +1,69 @@
+//USELESS IGNNORE THIS 
+// TRYING TO USE GOOGLE VISION NODE JS 
+
+const {Storage} = require('@google-cloud/storage');
+const automl = require('@google-cloud/automl');
+const fs = require('fs');
+const projectId = 'hip-apricot-264821'
+const keyFilename = './urbanforests-key.json'
+const storage = new Storage({projectId, keyFilename});
+console.log(`1`);
+
+
+// Instantiates a client. Explicitly use service account credentials by
+// specifying the private key file. All clients in google-cloud-node have this
+// helper, see https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
+// Makes an authenticated API request.
+console.log("0");
+try {
+const [buckets] = await storage.getBuckets();
+
+console.log('Buckets:');
+buckets.forEach(bucket => {
+    console.log(bucket.name);
+});
+} catch (err) {
+console.error('ERROR:', err);
+}
+console.log(`2`);
+
+// Create client for prediction service.
+const client = new automl.PredictionServiceClient();
+
+/**
+ * TODO(developer): Uncomment the following line before running the sample.
+ */
+// const projectId = `hip-apricot-264821`;
+// const computeRegion = `region-name, e.g. "us-central1"`;
+const modelId = `IOD6694191827760709632`;
+const filePath = `../public/request.json"`;
+// const scoreThreshold = `value between 0.0 and 1.0, e.g. "0.5"`;
+
+// Get the full path of the model.
+const modelFullId = client.modelPath(projectId, computeRegion, modelId);
+
+// Read the file content for prediction.
+const content = fs.readFileSync(filePath, 'base64');
+
+const params = {};
+console.log(`3`);
+if (scoreThreshold) {
+params.score_threshold = scoreThreshold;
+}
+
+// Set the payload by giving the content and type of the file.
+const payload = {};
+payload.image = {imageBytes: content};
+console.log(`4`);
+// params is additional domain-specific parameters.
+// currently there is no additional parameters supported.
+const [response] = await client.predict({
+name: modelFullId,
+payload: payload,
+params: params,
+});
+console.log(`Prediction results:`);
+response.payload.forEach(result => {
+console.log(`Predicted class name: ${result.displayName}`);
+console.log(`Predicted class score: ${result.classification.score}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,51 @@
+var exec = require('child_process').exec;
 const path = require('path');
 var express = require('express');
 var router = express.Router();
 
+
+async function sh(cmd) {
+  return new Promise(function (resolve, reject) {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ stdout, stderr });
+      }
+    });
+  });
+}
+
+async function main() {
+  let { stdout1} = await sh("if [ -f '/home/site/wwwroot/google-cloud-sdk/path.bash.inc' ]; then . '/home/site/wwwroot/google-cloud-sdk/path.bash.inc'; fi");
+  let { stdout2} = await sh("if [ -f '/home/site/wwwroot/google-cloud-sdk/completion.bash.inc' ]; then . '/home/site/wwwroot/google-cloud-sdk/completion.bash.inc'; fi");
+  let { stdout3} = await sh("source './google-cloud-sdk/path.bash.inc'");
+  let { stdout4} = await sh("source './google-cloud-sdk/completion.bash.inc'");
+  let { stdout5} = await sh('export PATH="./google-cloud-sdk/bin:$PATH"');
+  let { stdout6} = await sh("source './google-cloud-sdk/path.bash.inc'");
+  let { stdout } = await sh('export GOOGLE_APPLICATION_CREDENTIALS=./urbanforests-key.json\ncurl -X POST -H "Content-Type: application/json"   -H "Authorization: Bearer $(gcloud auth application-default print-access-token)"   https://automl.googleapis.com/v1beta1/projects/747099202913/locations/us-central1/models/IOD6694191827760709632:predict   -d @./public/request.json');
+  //let { stdout } = await sh('wc ./urbanforests-key.json');
+  //let { stdout } = await sh('gcloud --version');
+  //for (let line of stdout.split('\n')) {
+    //console.log(`${line}`);
+  //}
+  return stdout;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: "hello!"});
+ 
   res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+router.get('/analyze-image', function(req, res, next) {
+  main().then(result => {
+    console.log(result);
+    //res.sendFile(path.join(__dirname + "/test.html"));
+    res.send(result);
+  })
+  .catch(err => console.log(err))
 });
 
 module.exports = router;
